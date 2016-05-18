8a6e2deccdf1b9c0863c6d7a645dbeb16110bf1d
Madalin Ignisca <madalin.ignisca@gmail.com>
2016-05-18 13:15:08 +0100

    allow heroku to set the port

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ io.on('connection', function(socket) {
     });
 });
 
-http.listen(5000, function() {
-    console.log('listening on *:5000');
+http.listen((process.env.PORT || 5000), function() {
+    console.log('listening on *:' + (process.env.PORT || 5000));
 });
